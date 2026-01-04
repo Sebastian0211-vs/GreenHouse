6145aac21ee6c9ff4ac8908cfb5cd2c2dc339d3f
wgoni <raphael.gonin@hes-so.ch>
2026-01-04 15:33:58 +0100

    Added SQL queries  for dummy data insertion and main functionalities

---
+++ DB/Insert_data.sql
@@ -0,0 +1,104 @@
+INSERT INTO task_status (name) 
+VALUES
+('To do'),
+('Assigned'),
+('In progress'),
+('Overdue'),
+('Done');
+
+INSERT INTO task_types (name) 
+VALUES
+('Watering'),
+('Thin'),
+('Scout pest');
+('Planting');
+
+INSERT INTO beds (size) 
+VALUES
+(10),
+(16),
+(12);
+
+INSERT INTO units (unit) 
+VALUES
+('g'),
+('kg'),
+('pcs'),
+('ton');
+
+INSERT INTO suppliers (name, address, zip, city, phone) 
+VALUES
+('Jardinerie de Tourbillon', 'Rue de Tourbillon 5', '1950', 'Sion', '027 456 12 78'),
+('Jardinerie de Valère', 'Rue de Valère 18', '1950', 'Sion', '027 123 78 45');
+
+INSERT INTO users (pwd, username, first_name, last_name, active)
+VALUES
+('chaton1234', 'Jeanine', 'Jeanine', 'Matter', true),
+('Miz3rab1e', 'Jeannot', 'Jean', 'Valjean', true),
+('coe(è0£tç^5e7', 'TheGardener', 'Aristides', 'Madina', true);
+
+INSERT INTO items (name, supplier_id, unit_id)
+VALUES
+('Concombre Serpent de Chine', 1, 1),
+('Carotte Purple Haze', 2, 1),
+('Chou-fleur Romanesco', 2, 1),
+('Navet de Milan rouge', 1, 1),
+('Engrais', 2, 2),
+('Désherbant', 1, 2);
+
+INSERT INTO crops (item_id, variety, watering_interval, harvesting_window, thin_interval, scout_pest_interval)
+VALUES
+(1, 'Concombre Serpent de Chine', 1, 100, 100, 5),
+(2, 'Carotte Purple Haze', 1, 14, 14, 3),
+(3, 'Chou-fleur Romanesco', 2, 90, 90, 5),
+(4, 'Navet de Milan rouge', 2, 60, 60, 5);
+
+INSERT INTO stocks (item_id, stock_movement)
+VALUES
+(1, 1000),
+(2, 1000),
+(3, 1000),
+(4, 1000),
+(5, 450),
+(6, 250),
+(2, 800),
+(3, 500),
+(1, -80),
+(1, -120),
+(4, -110),
+(5, -12),
+(5, -20);
+
+INSERT INTO plantings (bed_id, crop_id, harvesting_date, is_trial, size)
+VALUES
+(1, 1, CURRENT_DATE + INTERVAL '100 DAY', false, 5),
+(1, 2, CURRENT_DATE + INTERVAL '14 DAY', false, 5),
+(2, 3, CURRENT_DATE + INTERVAL '90 DAY', false, 8),
+(2, 4, CURRENT_DATE + INTERVAL '60 DAY', true, 8);
+
+INSERT INTO notes (planting_id, text)
+VALUES
+(4, 'Jour 1, tout se passe bien'),
+(4, 'Jour 2, quelque chose semble bizarre, mais ce n''est probablement pas grave.'),
+(4, 'Jour 16, les plantes me parlent, venez me chercher s''il vous plait.');
+
+  INSERT INTO tasks (type_id, user_id, planting_id, 
+(1, 1, 1, CURRENT_DATE + INTERVAL '1 DAY'),
+(1, 1, 1, CURRENT_DATE + INTERVAL '2 DAY'),
+(1, 2, 1, CURRENT_DATE + INTERVAL '3 DAY'),
+(1, 2, 1, CURRENT_DATE + INTERVAL '4 DAY'),
+(1, 3, 2, CURRENT_DATE + INTERVAL '1 DAY'),
+(1, 3, 2, CURRENT_DATE + INTERVAL '2 DAY'),
+(1, 3, 2, CURRENT_DATE + INTERVAL '3 DAY'),
+(1, NULL, 3, CURRENT_DATE + INTERVAL '4 DAY'),
+(1, NULL, 3, CURRENT_DATE + INTERVAL '5 DAY'),
+(1, NULL, 3, CURRENT_DATE + INTERVAL '6 DAY'),
+(2, NULL, 3, CURRENT_DATE + INTERVAL '4 DAY'),
+(3, NULL, 3, CURRENT_DATE + INTERVAL '5 DAY'),
+(4, NULL, 3, CURRENT_DATE + INTERVAL '6 DAY');
+
+INSERT INTO task_materials (task_id, item_id, qty)
+VALUES
+(8, 6, 1),
+(9, 6, 2),
+(10, 6, 1);
---
+++ DB/Main_queries.sql
@@ -0,0 +1,293 @@
+-- SELECT all tasks with or without user_id parameter:
+SELECT
+  u.username, 
+  tt.name || ' of planting n.' || t.planting_id || ', variety: ' || c.variety AS Task,
+  'Status: ' || ts.name AS Status, 
+  'Due: ' || TO_CHAR(t.due, 'DD.MM.YYYY') AS Due 
+FROM
+  tasks AS t
+JOIN
+  task_types AS tt ON t.type_id = tt.id
+JOIN
+  task_status AS ts ON t.status_id = ts.id
+JOIN
+  plantings AS p ON t.planting_id = p.id
+JOIN
+  crops AS c ON p.crop_id = c.id
+LEFT JOIN
+  users AS u ON t.user_id = u.id
+-- Filter to select a specific one:
+WHERE
+  t.user_id = COALESCE($1, t.user_id) OR ($1 IS NULL AND t.user_id IS NULL)
+-- -- ** -- --
+
+
+-- SELECT all plantings:
+SELECT
+  c.variety,
+  p.planting_date,
+  p.harvesting_date,
+  p.size,
+  p.is_trial,
+  p.id AS bed_number
+FROM
+  plantings AS p
+JOIN
+  crops AS c ON p.crop_id = c.id
+-- Filter to select a specific one:
+WHERE
+  p.id = COALESCE($1, p.id) OR
+  ($1 IS NULL AND p.id IS NULL)
+-- -- ** -- --
+
+
+-- SELECT beds list with available space:
+WITH used_space AS(
+  SELECT
+    SUM(p.size) AS used_space_per_bed,
+    p.bed_id
+  FROM
+    plantings AS p
+  GROUP BY
+    p.bed_id
+)
+SELECT
+  b.id AS bed_number,
+  us.used_space_per_bed AS used_space,
+  b.size AS total_size
+FROM
+  beds AS b
+JOIN
+  used_space AS us ON b.id = us.bed_id
+-- -- ** -- --
+
+
+-- SELECT stock quantities
+WITH stock_qty AS(
+  SELECT
+    s.item_id,
+    SUM(s.stock_movement) AS qty
+  FROM
+    stocks AS s
+  GROUP BY
+    s.item_id
+)
+SELECT
+  i.name,
+  sq.qty,
+  u.unit
+FROM
+  items AS i
+JOIN
+  units AS u ON i.unit_id = u.id
+JOIN
+  stock_qty AS sq ON i.id = sq.item_id
+-- -- ** -- --
+
+
+-- SELECT task supplies
+SELECT
+  t.id,
+  i.name,
+  tm.qty,
+  u.unit
+FROM
+  items AS i
+JOIN
+  task_materials AS tm ON tm.item_id = i.id
+JOIN
+  units AS u ON i.unit_id = u.id
+JOIN
+  tasks AS t ON tm.task_id = t.id
+-- Filter by task:
+WHERE
+  t.id = COALESCE($1, t.id) OR
+  ($1 IS NULL AND t.id IS NULL)
+-- -- ** -- --
+
+
+-- SELECT notes
+SELECT
+  p.id AS planting_number,
+  p.bed_id AS bed_number,
+  c.variety,
+  n.text AS note,
+  n.datetime
+FROM
+  notes AS n
+JOIN
+  plantings AS p ON n.planting_id = p.id
+JOIN
+  crops AS c ON p.crop_id = c.id
+-- Filter by planting:
+WHERE
+  p.id = COALESCE($1, p.id) OR
+  ($1 IS NULL AND p.id IS NULL)
+-- -- ** -- --
+
+
+-- SELECT units
+SELECT
+  id,
+  unit
+FROM
+  units
+-- -- ** -- --
+
+
+-- SELECT suppliers
+SELECT
+  id,
+  name
+FROM
+  suppliers
+-- -- ** -- --
+
+
+-- UPDATE task at assignment by a user
+UPDATE tasks
+SET
+  user_id = $2,
+  due = GREATEST(due, CURRENT_DATE + 1),
+  status_id = 2
+WHERE
+  id = $1
+-- -- ** -- --
+
+
+-- UPDATE task at start
+UPDATE tasks
+SET
+  start_time = LOCALTIMESTAMP,
+  due = GREATEST(due, CURRENT_DATE + 1),
+  status_id = 3
+WHERE
+  id = $1
+-- -- ** -- --
+
+
+-- UPDATE task when completed AND UPDATE stock quantities
+WITH update_task AS(
+  UPDATE tasks
+  SET
+    end_time = LOCALTIMESTAMP,
+    due = GREATEST(due, CURRENT_DATE + 1),
+    status_id = 5
+  WHERE
+    id = $1
+  RETURNING id
+)
+-- Then INSERT new stock line
+INSERT INTO stocks(
+  item_id,
+  stock_movement
+)
+SELECT
+  i.id,
+  -tm.qty
+FROM
+  items AS i
+JOIN
+  task_materials AS tm ON tm.item_id = i.id
+JOIN
+  update_task AS ut ON tm.task_id = ut.id
+-- -- ** -- --
+
+
+-- UPDATE stock quantities upon order reception
+INSERT INTO stocks(
+  item_id,
+  stock_movement
+)
+VALUES(
+  $1,
+  $2
+)
+-- -- ** -- --
+
+
+-- INSERT new plantings (possibly add all tasks, maybe too complicated in SQL, tbd)
+INSERT INTO plantings(
+  bed_id,
+  crop_id,
+  harvesting_date,
+  is_trial,
+  size
+  )
+SELECT
+  $1,
+  $2,
+  CURRENT_DATE + c.harvesting_window,
+  $3,
+  $4
+FROM
+  crops AS c
+WHERE
+  c.id = $2 AND
+  $4 <= (
+    SELECT
+      b.size - COALESCE(SUM(p.size), 0)
+    FROM
+      beds b
+    LEFT JOIN
+      plantings p ON p.bed_id = b.id
+    WHERE
+      b.id = $1
+    GROUP BY
+      b.size
+  )
+RETURNING id
+-- RETURNING statement is to be used to check if the insertion was successful:
+-- (id != null => enough space, id == null => not enough space in selected  bed)
+-- app must listen to that and react accordingly
+-- -- ** -- --
+
+
+-- INSERT new user
+INSERT INTO users(
+  pwd,
+  username,
+  first_name,
+  last_name
+)
+VALUES(
+  $1,
+  $2,
+  $3,
+  $4
+)
+-- -- ** -- --
+
+
+-- INSERT new crop variety
+WITH new_items AS(
+  INSERT INTO items(
+    name,
+    supplier_id,
+    unit_id
+  )
+  VALUES(
+    $1,
+    $6,
+    $7
+  )
+  RETURNING id
+)
+INSERT INTO crops(
+  item_id,
+  variety,
+  watering_interval,
+  harvesting_window,
+  thin_interval,
+  scout_pest_interval
+)
+SELECT
+  id,
+  $1,
+  $2, 
+  $3,
+  $4,
+  $5
+FROM 
+  new_items
+-- -- ** -- --
